9ce0a4898103e55a0bd8752648127101e0bfe93a
Aleksandr Yurchenko <y.aleksandr89@yandex.ru>
2026-03-14 14:48:32 +0300

    Добавлен шаблонизатор, базовый layout и шаблон главной страницы с Bootstrap

--- public/index.php
+++ public/index.php
@@ -5,17 +5,15 @@ require __DIR__ . '/../vendor/autoload.php';
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Slim\Views\PhpRenderer;
 
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response) {
-    $response->getBody()->write('Page Analyzer');
-    return $response;
-});
+$renderer = new PhpRenderer(__DIR__ . '/../templates');
+$renderer->setLayout('layout.phtml');
 
-// TODO: временное решение, что бы не кидало исключение
-$app->get('/favicon.ico', function (Request $request, Response $response) {
-    return $response->withStatus(204);
+$app->get('/', function (Request $request, Response $response) use ($renderer) {
+    return $renderer->render($response, 'index.phtml');
 });
 
 $app->run();
