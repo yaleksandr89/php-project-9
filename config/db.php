43d9809761afc6199b8422176efe5652a1b1aa8e
Aleksandr Yurchenko <y.aleksandr89@yandex.ru>
2026-04-09 02:49:46 +0300

    Исправлено подключение к БД (учтен порт по умолчанию)

--- config/db.php
+++ config/db.php
@@ -2,13 +2,15 @@
 
 function getPDO(): PDO
 {
-    $databaseUrl = parse_url(getenv('DATABASE_URL'));
+    $databaseUrl = parse_url(
+        getenv('DATABASE_URL')
+    );
 
-    $host = $databaseUrl['host'];
-    $port = $databaseUrl['port'];
-    $dbName = ltrim($databaseUrl['path'], '/');
-    $username = $databaseUrl['user'];
-    $password = $databaseUrl['pass'];
+    $host = $databaseUrl['host'] ?? 'localhost';
+    $port = $databaseUrl['port'] ?? 5432;
+    $dbName = isset($databaseUrl['path']) ? ltrim($databaseUrl['path'], '/') : '';
+    $username = $databaseUrl['user'] ?? '';
+    $password = $databaseUrl['pass'] ?? '';
 
     $pdo = new PDO(
         "pgsql:host={$host};port={$port};dbname={$dbName}",
