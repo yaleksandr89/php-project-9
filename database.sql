07684c2a9260fb1c6e39713a9c077c74e2a60271
Aleksandr Yurchenko <y.aleksandr89@yandex.ru>
2026-04-10 00:10:50 +0300

    Добавлена таблица url_checks для хранения проверок URL

--- database.sql
+++ database.sql
@@ -1,6 +1,17 @@
 CREATE TABLE urls
 (
-    id         BIGSERIAL       PRIMARY KEY,
-    name       VARCHAR(255)    UNIQUE NOT NULL,
-    created_at TIMESTAMP       NOT NULL
+    id         BIGSERIAL PRIMARY KEY,
+    name       VARCHAR(255) UNIQUE NOT NULL,
+    created_at TIMESTAMP NOT NULL
+);
+
+CREATE TABLE url_checks
+(
+    id          BIGSERIAL PRIMARY KEY,
+    url_id      BIGINT NOT NULL REFERENCES urls (id) ON DELETE CASCADE,
+    status_code INTEGER,
+    h1          TEXT,
+    title       TEXT,
+    description TEXT,
+    created_at  TIMESTAMP NOT NULL
 );
